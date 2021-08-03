450497d44ecdbd4569e4163904d7f5fc6c88cc23
King <guancyxx@guancyxx.cn>
2021-08-03 10:03:05 +0800

    feat: 🎸 添加bind和search方法

--- examples/test-client.js
+++ examples/test-client.js
@@ -3,10 +3,10 @@ var client = ldap.createClient({
   url: 'ldap://127.0.0.1:1389'
 });
 
-client.bind('CN=admin,OU=admin', 'admin', function(err) {
+client.bind('uid=guancy,ou=people, o=TENANT_ID, dc=longguikeji, dc=com', 'Guancyxx2021**', function (err) {
   if (err) {
     console.log('Bind Error:', JSON.stringify(err, null, 2));
-    return client.unbind(function(err) {
+    return client.unbind(function (err) {
       if (err) {
         console.log(err);
       }
@@ -16,24 +16,24 @@ client.bind('CN=admin,OU=admin', 'admin', function(err) {
   console.log('Bind success.');
 
   var opts = {
-    filter: '(email=admin@fabrikam.com)',
+    filter: '(username=guancy)',
     scope: 'sub',
     attributes: ['dn', 'sn', 'cn']
   };
 
   console.log(`Searching for: ${JSON.stringify(opts, null, 2)}`);
 
-  client.search('OU=Username-Password-Authentication', opts, function(err, res) {
-    res.on('searchEntry', function(entry) {
+  client.search('OU=people,o=392fbf1b9931492483d84c7694147e61', opts, function (err, res) {
+    res.on('searchEntry', function (entry) {
       console.log('Found: ' + JSON.stringify(entry.object));
     });
-    res.on('searchReference', function(referral) {
+    res.on('searchReference', function (referral) {
       console.log('Referral: ' + referral.uris.join());
     });
-    res.on('error', function(err) {
+    res.on('error', function (err) {
       console.error('Error: ' + err.message);
     });
-    res.on('end', function(result) {
+    res.on('end', function (result) {
       console.log('Search Done. Status: ' + result.status);
     });
   });
