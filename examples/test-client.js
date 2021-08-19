7ad40b5e84de6060d59e1bba055ef1b383e86ff9
King <guancyxx@guancyxx.cn>
2021-08-19 17:46:29 +0800

    fix: 🐛 处理dn为空的BUG
    dn为空时将其置为o一级处理

--- examples/test-client.js
+++ examples/test-client.js
@@ -3,7 +3,7 @@ var client = ldap.createClient({
   url: 'ldap://127.0.0.1:1389'
 });
 
-client.bind('cn=admin,ou=people, o=392fbf1b9931492483d84c7694147e61, dc=longguikeji, dc=com', 'admin', function (err) {
+client.bind('cn=admin,ou=people, o=4a883ea3e16d49fcbb814b27799343e7, dc=longguikeji, dc=com', 'admin', function (err) {
   if (err) {
     console.log('Bind Error:', JSON.stringify(err, null, 2));
     return client.unbind(function (err) {
@@ -23,7 +23,21 @@ client.bind('cn=admin,ou=people, o=392fbf1b9931492483d84c7694147e61, dc=longguik
 
   console.log(`Searching for: ${JSON.stringify(opts, null, 2)}`);
 
-  client.search(`ou=people`, opts, function (err, res) {
+  client.search(``, opts, function (err, res) {
+    res.on('searchEntry', function (entry) {
+      console.log('Found: ' + JSON.stringify(entry.object));
+    });
+    res.on('searchReference', function (referral) {
+      console.log('Referral: ' + referral.uris.join());
+    });
+    res.on('error', function (err) {
+      console.error('Error: ' + err.message);
+    });
+    res.on('end', function (result) {
+      console.log('Search Done. Status: ' + result.status);
+    });
+  });
+  client.search(`ou=people, o=4a883ea3e16d49fcbb814b27799343e7, dc=longguikeji, dc=com`, opts, function (err, res) {
     res.on('searchEntry', function (entry) {
       console.log('Found: ' + JSON.stringify(entry.object));
     });
