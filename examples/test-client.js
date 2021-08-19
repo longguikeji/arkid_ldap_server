0f1318585828ddae7babff29ec6734eea0ccf4b0
King <guancyxx@guancyxx.cn>
2021-08-19 18:06:04 +0800

    fix: 🐛 处理过滤参数大小写问题

--- examples/test-client.js
+++ examples/test-client.js
@@ -16,27 +16,13 @@ client.bind('cn=admin,ou=people, o=4a883ea3e16d49fcbb814b27799343e7, dc=longguik
   console.log('Bind success.');
 
   var opts = {
-    filter: '(cn=admin)',
+    filter: '(givenName=Lee)',
     scope: 'sub',
     attributes: ['dn', 'sn', 'cn']
   };
 
   console.log(`Searching for: ${JSON.stringify(opts, null, 2)}`);
 
-  client.search(``, opts, function (err, res) {
-    res.on('searchEntry', function (entry) {
-      console.log('Found: ' + JSON.stringify(entry.object));
-    });
-    res.on('searchReference', function (referral) {
-      console.log('Referral: ' + referral.uris.join());
-    });
-    res.on('error', function (err) {
-      console.error('Error: ' + err.message);
-    });
-    res.on('end', function (result) {
-      console.log('Search Done. Status: ' + result.status);
-    });
-  });
   client.search(`ou=people, o=4a883ea3e16d49fcbb814b27799343e7, dc=longguikeji, dc=com`, opts, function (err, res) {
     res.on('searchEntry', function (entry) {
       console.log('Found: ' + JSON.stringify(entry.object));
@@ -51,4 +37,18 @@ client.bind('cn=admin,ou=people, o=4a883ea3e16d49fcbb814b27799343e7, dc=longguik
       console.log('Search Done. Status: ' + result.status);
     });
   });
+  // client.search(`o=4a883ea3e16d49fcbb814b27799343e7, dc=longguikeji, dc=com`, opts, function (err, res) {
+  //   res.on('searchEntry', function (entry) {
+  //     console.log('Found: ' + JSON.stringify(entry.object));
+  //   });
+  //   res.on('searchReference', function (referral) {
+  //     console.log('Referral: ' + referral.uris.join());
+  //   });
+  //   res.on('error', function (err) {
+  //     console.error('Error: ' + err.message);
+  //   });
+  //   res.on('end', function (result) {
+  //     console.log('Search Done. Status: ' + result.status);
+  //   });
+  // });
 });
